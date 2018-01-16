243b3825a1403ce61d0b1d4613bd5a68009d2212
M.C. van den Boogaart <scola@kvdb.name>
2018-01-16 21:00:31 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+import setupClient from './src/helper/setup-client';
+import setupServer from './src/helper/setup-server';
 import chunked from './src/chunked';
 import json from './src/json';
 import msgpack from './src/msgpack';
 import urlencoded from './src/urlencoded';
-import setupClient from './src/helper/setup-client';
-import setupServer from './src/helper/setup-server';
+
+export {
+  setupClient,
+  setupServer
+};
 
 export {
   chunked,
   json,
   msgpack,
-  urlencoded,
-  setupClient,
-  setupServer
+  urlencoded
 };
